b3a8436ef65bd611adb3383d92e4e8e1fff98322
akzain <zainkamaal66885@gmail.com>
2021-06-25 19:09:10 +0300

    fixed some problem regardin responsiveness

--- ping-coming-soon-page-master/js.js
+++ ping-coming-soon-page-master/js.js
@@ -1,23 +1,15 @@
-function validateEmail(email) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
+const subscriptionForm = document.getElementById("form");
+const emailInput = document.getElementById("email-input");
+const errorMessage = document.getElementById("error-message");
 
-function validate() {
-  const $result = $("#result");
-  const email = $("#email").val();
-  $result.text("");
+const showError = (element) => {
+  element.classList.add("error");
+  errorMessage.classList.remove("hide");
+};
 
-  if (validateEmail(email)) {
-    $("#validate").reset();
-  } else {
-    $(".input").addClass("v");
-    setTimeout(() => {
-      $(".input").removeClass("v");
-    }, 5000);
+subscriptionForm.addEventListener("submit", (event) => {
+  if (!emailInput.validity.valid) {
+    showError(emailInput);
+    event.preventDefault();
   }
-  return false;
-}
-
-$("#validate").on("click", validate);
+});
